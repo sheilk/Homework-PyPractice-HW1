8b83f0685868dbf3fa1f6d6657373381feb46069
ACatInTheTree <75489142+sheilk@users.noreply.github.com>
2023-10-08 00:19:29 +0800

    second commit
    Add a function of getting BibTeX from dblp

--- myutils.py
+++ myutils.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import bibtexparser
 import os
-
+from paperdinfo import*
 
 class DownPDF:
     def __init__(self, down_folder_path, reference):
@@ -27,4 +27,42 @@ class DownPDF:
         with open(file_path, 'wb') as file:
             file.write(pdf.content)
 
+class Downfile:
+    def __init__(self,pdf_reader,down_path,filetype = 1):
+        # filetype: bibtex is 1, pdf is 0.
+        self.pdf = pdf_reader
+        self.path = down_path
+        self.filetype = filetype 
+        self.bibtexes = []
 
+    def getbibfromDBLP(self):
+        for ref in self.pdf.ref_titles:
+            bibtex = ""
+            url = f"https://dblp.org/search?q={ref}&h=1000&f=0&t=o"
+            headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"}
+            search_res = requests.get(url,headers = headers)
+            soup = BeautifulSoup(search_res.content, "html.parser")
+            nav_e = soup.find('nav', class_='publ')
+            if(nav_e == None):
+                continue
+            bibtex_url = nav_e.find("a", {"rel": "nofollow"}).get("href")
+            
+            bib_res = requests.get(bibtex_url,headers=headers)
+            soup2 = BeautifulSoup(bib_res.content,"html.parser")
+            div_e = soup2.find('div', id='bibtex-section')
+            pre_e = div_e.find('pre', class_='verbatim select-on-click')
+            bibtex = pre_e.get_text(strip=True)
+            self.bibtexes.append(bibtex)
+            
+        # test
+        with open('output.bib', 'w') as bibfile:
+            for bibtex in self.bibtexes:
+                bibfile.write(bibtex + '\n')
+        
+
+# Debug
+# path = "C:/Users/liutr/Desktop/1"
+# pdf_reader = PDFReader("C:/Users/liutr/Desktop/demo.pdf")
+# pdf_reader.read_pdf()
+# downfile = Downfile(pdf_reader,path)
+# downfile.getbibfromDBLP()
--- paperdinfo.py
+++ paperdinfo.py
@@ -1,5 +1,5 @@
 import fitz
-
+import re
 
 class PDFReader:
     def __init__(self, file_path):
@@ -7,6 +7,8 @@ class PDFReader:
         self.title = ""
         self.firstauthor = ""
         self.references = []
+        self.ref_titles = []
+        self.ref_num    = []
 
     def read_pdf(self):
         # Title
@@ -18,28 +20,36 @@ class PDFReader:
         # FirstAuthor
         self.firstauthor = blocks[1]['lines'][0]['spans'][0]['text']
         # References
-        ref_index = 0   
-        for page_index, page in enumerate(doc):
-            blocks = page.get_text("dict")["blocks"]
-            if blocks[0].get('lines') and blocks[0]['lines'][0].get('spans'):
-                if 'Reference' in blocks[0]['lines'][0]['spans'][0]['text']:
-                    ref_index = page_index
-                    break
-        for i in range(ref_index, doc.page_count):
-            page = doc[i]
-            blocks = page.get_text("dict")["blocks"]
-            for block in blocks:
-                reference = ""
-                for line in block["lines"]:
-                    for span in line["spans"]:
-                        reference += span["text"]
-                self.references.append(reference)
-        self.references = self.references[1:]
-            
+        text = ""
+        find_ref = False
+        for page in doc:
+            text_content = page.get_text()
+            if find_ref == True:
+                text += text_content
+            if "References\n" in text_content:
+                text += text_content.split("References\n")[1]
+                find_ref = True
 
+        # Get each reference
+        text = text.replace('-\n', '\n')
+        for s in text.split('[')[1:]:
+            self.references.append(s.split(']')[1].replace('\n', ' ').strip().rsplit('.', 1)[0] + '.')
+        
+        self.ref_num = len(self.references)
+        # Get each reference_title
+        for ref in self.references:
+            temp = ref
+            temp = re.sub(r'(?<=\d)\.(?=\d)', '', temp) 
+            self.ref_titles.append(temp.rsplit('.')[-3].strip())
+        
 # Debug
-# pdf_reader = PDFReader("C:/Users/liutr/Desktop/v1.pdf")
+# pdf_reader = PDFReader("C:/Users/liutr/Desktop/demo.pdf")
 # pdf_reader.read_pdf()
 # print("标题:", pdf_reader.title)
 # print("第一个作者:", pdf_reader.firstauthor)
 # print("参考文献:", pdf_reader.references)
+# print("参考文献标题:", pdf_reader.ref_titles)
+# i = 1
+# for s in pdf_reader.ref_titles:
+#     print(f"{i}:" + s)
+#     i+=1
